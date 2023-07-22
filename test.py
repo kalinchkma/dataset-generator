43e070f56b1251d9b1f5f8982acdca33a524b37e
nanashi10211 <chakma.kalin10211@gmail.com>
2023-07-23 00:20:39 +0600

    fixed image shape issue

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from rembg import removeBackground
 
 ## test bg remove
-# removeBackground(sourceFolder="test/test_images/", destinationFolder="test/results/")
-l = ["hunter", "lol"]
-if "hunter" not in l:
-    print("hunter not in")
-else:
-    print("it's in the list")
+removeBackground(sourceFolder="test/test_images/", destinationFolder="test/results/")
+# l = ["hunter", "lol"]
+# if "hunter" not in l:
+#     print("hunter not in")
+# else:
+#     print("it's in the list")
